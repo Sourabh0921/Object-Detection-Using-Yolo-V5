52bc6e47ffea84f9abe84ebd9991610e57100e9d
Sourabh Kadam <72154096+Sourabh0921@users.noreply.github.com>
2023-11-06 13:03:48 +0530

    Setup and Requirements Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Columns_Headers_Detection',
+    version= '0.0.0',
+    author= 'Sourabh0921',
+    author_email= 'saurabhkadam612a@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
